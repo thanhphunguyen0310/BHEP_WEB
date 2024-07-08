baefe838d3d69a8bb0d76934a217acac4efbc101
Nguyễn Thành Phú <thanhphupt555@gmail.com>
2024-07-08 09:13:15 +0700

    change padding banner, getFontSize at user health card

--- src/components/Customer/BodyTemperatureCard.jsx
+++ src/components/Customer/BodyTemperatureCard.jsx
@@ -2,10 +2,16 @@ import { Card, Col, Image, Row, Typography } from "antd";
 import TemperatureIcon from "../../assets/icon/body_temperature.svg";
 
 const TemperatureCard = ({ data }) => {
+  const getFontSize = (data) => {
+    if (data.length >= 3) {
+      return "20px"; // Kích thước nhỏ hơn nếu dữ liệu dài
+    }
+    return "25px"; // Kích thước mặc định
+  };
   return (
     <Card
       hoverable
-      style={{border:"1px solid #3285a8"}}
+      style={{ border: "1px solid #3285a8" }}
       title={
         <>
           <Typography.Title level={5}>Nhiệt độ</Typography.Title>
@@ -14,7 +20,14 @@ const TemperatureCard = ({ data }) => {
     >
       <Row justify={"space-between"} align={"middle"} className="health-index">
         <Col span={12}>
-          <Typography.Text style={{fontWeight:"600", fontSize:"25px"}}>{data} °C</Typography.Text>
+          <Typography.Text
+            style={{
+              fontWeight: "600",
+              fontSize: getFontSize(data.toString()), // Áp dụng kích thước font
+            }}
+          >
+            {data} °C
+          </Typography.Text>
         </Col>
         <Col span={8}>
           <Image
--- src/components/Customer/SpO2Card.jsx
+++ src/components/Customer/SpO2Card.jsx
@@ -2,10 +2,16 @@ import { Card, Col, Image, Row, Typography } from "antd";
 import SpO2Icon from "../../assets/img/spo2.png";
 
 const SpO2Card = ({ data }) => {
+  const getFontSize = (data) => {
+    if (data.length >= 3) {
+      return "20px"; // Kích thước nhỏ hơn nếu dữ liệu dài
+    }
+    return "25px"; // Kích thước mặc định
+  };
   return (
     <Card
       hoverable
-      style={{border:"1px solid #3285a8"}}
+      style={{ border: "1px solid #3285a8" }}
       title={
         <>
           <Typography.Title level={5}>Chỉ số SpO2</Typography.Title>
@@ -14,7 +20,14 @@ const SpO2Card = ({ data }) => {
     >
       <Row justify={"space-between"} align={"middle"} className="health-index">
         <Col span={12}>
-          <Typography.Text style={{fontWeight:"600", fontSize:"25px"}}>{data} %</Typography.Text>
+          <Typography.Text
+            style={{
+              fontWeight: "600",
+              fontSize: getFontSize(data.toString()), // Áp dụng kích thước font
+            }}
+          >
+            {data} %
+          </Typography.Text>
         </Col>
         <Col span={8}>
           <Image
